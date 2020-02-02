d6171ceece5409ca7d9d2efbbd292270b040ebe7
erricov <elyerrico@gmail.com>
2020-02-02 17:02:42 +0100

    Vue component card-item mejorado 3

---
+++ components/header.vue
@@ -0,0 +1,28 @@
+<!-- Header Start -->
+<template>
+	<nav class="navbar navbar-expand-lg navbar-light bg-light shadow-sm p-3 px-md-4">
+  		<a class="navbar-brand" href="#">Ely's source</a>
+  		<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+    		<span class="navbar-toggler-icon"></span>
+  		</button>
+
+	  <div class="collapse navbar-collapse" id="navbarSupportedContent">
+	    <ul class="navbar-nav ml-auto mr-md-3">
+	      <li class="nav-item active">
+	        <a class="nav-link" href="#">Home <span class="sr-only">(current)</span></a>
+	      </li>
+	      <li class="nav-item">
+	        <a class="nav-link" href="#">Services</a>
+	      </li>
+	      <li class="nav-item">
+	        <a class="nav-link" href="#" tabindex="-1" aria-disabled="true">About</a>
+	      </li>
+	      <li class="nav-item">
+	        <a class="nav-link" href="#">Contact</a>
+	      </li>
+	    </ul>
+	  </div>
+       <a class="btn btn-outline-success " href="#">Sign up</a>
+	</nav>
+</template>
+<!-- Header End -->
